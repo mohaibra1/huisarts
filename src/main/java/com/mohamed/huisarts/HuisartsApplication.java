319315e60fe87b6ea287324945da52a3f92407ef
mimoh <mimoh@DESKTOP-I23K3L6>
2020-02-28 23:12:51 +0100

    project huis eerste commit

---
+++ src/main/java/com/mohamed/huisarts/HuisartsApplication.java
@@ -0,0 +1,13 @@
+package com.mohamed.huisarts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuisartsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuisartsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mohamed/huisarts/HuisartsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohamed.huisarts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuisartsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
